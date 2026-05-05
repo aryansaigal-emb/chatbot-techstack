945ea77e044ca59ad5d45592e7d263090e058abb
aryansaigal-emb <aryan.saigal@emb.global>
2026-05-05 17:46:06 +0530

    Pass Cloudflare secrets to backend container

--- backend/worker.js
+++ backend/worker.js
@@ -1,8 +1,15 @@
 import { Container, getContainer } from '@cloudflare/containers'
+import { env } from 'cloudflare:workers'
 
 export class ChatbotBackend extends Container {
   defaultPort = 8080
   sleepAfter = '10m'
+  envVars = {
+    GROQ_API_KEY: env.GROQ_API_KEY,
+    SUPABASE_URL: env.SUPABASE_URL,
+    SUPABASE_KEY: env.SUPABASE_KEY,
+    FRONTEND_ORIGINS: env.FRONTEND_ORIGINS,
+  }
 }
 
 export default {
